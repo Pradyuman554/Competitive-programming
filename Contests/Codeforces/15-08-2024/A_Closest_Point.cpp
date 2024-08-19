3a361d31bf25f6bbed9a10491b64fffaf0dbc498
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-19 08:37:43 +0530

    game w d

--- Contests/Codeforces/15-08-2024/A_Closest_Point.cpp
+++ Contests/Codeforces/15-08-2024/A_Closest_Point.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int t; 
     cin >> t;
-    //
+
     while(t--){
         long long n;  cin>>n;
         vector<long long> arr(n);
--- Contests/Codeforces/15-08-2024/B_Game_with_Doors.cpp
+++ Contests/Codeforces/15-08-2024/B_Game_with_Doors.cpp
@@ -1,18 +1,4 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<cmath>
-#include<map>
-#include<set>
-#include<queue>
-#include<stack>
-#include<deque>
-#include<climits>
-#include<cstring>
-#include<cassert>
-#include<unordered_map>
-#include<unordered_set>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -28,19 +14,67 @@ int main() {
             continue;
         }
         //If disjoint
-        if( (x1<x2 && y1<x2) || (x1>x2 && x1>y2) ){
+        
+        if( x2>y1 || x1>y2 ){
             cout<<1<<endl;
             continue;
         }
 
-        int end = min(y1,y2);
-        int start = max(x1,x2);
-        int extra=0;
-        if(end<max(y1,y2))  extra++;
-        if(start>min(x1,x2))    extra++;
-        int inter = end-start+extra;
-        cout<<inter<<endl;
+        int end = min(y1, y2);
+        int start = max(x1, x2);
+        int inter = end - start+1; // Calculate the intersection length
+
+        cout << inter << endl;
     }
 
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        if (x1 > y2 || x2 > y1)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            int res = 1;
+            if (x1 > x2)
+            {
+                swap(x1, x2);
+                swap(y1, y2);
+            }
+            if (y2 <= y1)
+            {
+                res += y2 - x2 + 1;
+            }
+            else
+            {
+                res += y1 - x2 + 1;
+            }
+            if (x1 == x2)
+            {
+                res--;
+            }
+            if (y2 == y1)
+            {
+                res--;
+            }
+            cout << res << endl;
+        }
+    }
+    return 0;
+}
+*/

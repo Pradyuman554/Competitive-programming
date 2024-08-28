734a4d2b18a45840c5cb6f3ed5f6477e83900404
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-28 08:19:45 +0530

    Upsolve

--- Contests/Codeforces/25-08-2024/C_Turtle_and_Good_Pairs.cpp
+++ Contests/Codeforces/25-08-2024/C_Turtle_and_Good_Pairs.cpp
@@ -1,28 +1,51 @@
+//We want the same digits TO BE FAR from each, as far as they can be coz for 2 letters to be a pleasant pair IF ONE OF THE MIDDLE 2 ELEMENTS IS DIFFERENT FROM THE bounding pair elements
+/*
+ddedf
+Counting good pairs:
+Same ones:
+1. dd
+2. dd
+3. dd
+Pleasant:
+4. ddedf
+5. dedf
+6. edf
+
+So. 6
+
+For dfded
+1. dfd
+2. dfde
+3. dfded
+4. fde
+5. fded
+6. ded
+
+So, 6, BUT UK WE GET THE IDEA. Similar elements need to be AS FAR AS POSSIBLE, otherwise pleasant vale main same aa jayega
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define mod 1000000007
-void solve()
+int main()
 {
-    int n;  cin>>n;
-    string s;
-    cin>>s;
-    sort(s.begin(), s.end());
-    cout<<s<<endl;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-//#ifndef ONLINE_JUDGE
-//	freopen("input.txt", "r", stdin);
-//	freopen("output.txt", "w", stdout);
-//#endif
-
-    ll test=1;
-    cin>>test;
-    while(test--)
+    ll t;
+    cin >> t;
+    while (t--)
     {
-        solve();
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        ll l = 0, r = n - 1;
+        while (l < r)
+        {
+            cout << s[l] << s[r];
+            l++, r--;
+        }
+        if (l == r)
+            cout << s[l];
+        cout << endl;
     }
     return 0;
 }
